380da06f8170d1c28c07f368ba026640d0ca6e0d
Alyssa Gerardo <alyssagerardo2005@gmail.com>
2025-09-30 13:30:16 -0700

    final commit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -4,24 +4,28 @@ import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
- const [characters, setCharacters] = useState([]);
-
-  return (
-    <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
-      <Form /> 
-    </div>
-  );
+ const [characters, setCharacters] = useState([]); 
 
 function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
     setCharacters(updated);
-  }
+}
+
+function updateList(person) {
+  setCharacters([...characters, person]);
+}
+
+return (
+  <div className="container">
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+    />
+    <Form handleSubmit={updateList} />
+  </div>
+);
 }
 
 export default MyApp;
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -18,10 +18,9 @@ function TableBody(props) {
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
-	<td>
-	  <button onClick={() => props.removeCharacter(index)}>
-	    Delete
-	  </button>
+        <td> <button onClick={() => props.removeCharacter(index)}>
+               Delete
+             </button>
         </td>
       </tr>
     );

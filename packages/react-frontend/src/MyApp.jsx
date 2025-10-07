17a6f2cffed967a7951524e492416debdd95af8b
Alyssa Gerardo <alyssagerardo2005@gmail.com>
2025-10-07 13:26:32 -0700

    POST added

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,4 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
 import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
@@ -15,7 +14,11 @@ function removeOneCharacter(index) {
 }
 
 function updateList(person) {
-  setCharacters([...characters, person]);
+  postUser(person) 
+	.then(() => setCharacters([...characters, person]))
+	.catch((error) => {
+ 	  console.log(error);
+	})
 }
 
 function fetchUsers() {
@@ -29,6 +32,18 @@ useEffect(() => {
 	.then((json) => setCharacters(json["users_list"]))
 	.catch((error) => { console.log(error); });
 }, [] ); 
+
+function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", { 
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json", 
+      }, 
+      body: JSON.stringify(person), 
+   }); 
+
+   return promise; 
+}
 
 return (
   <div className="container">

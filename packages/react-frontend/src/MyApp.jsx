ba3ac017316918da6db73817ed53b3062c4ea312
Alyssa Gerardo <alyssagerardo2005@gmail.com>
2025-10-24 23:52:30 -0700

    Fixed npm run dev from the root

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -14,7 +14,46 @@ function removeOneCharacter(index) {
 }
 
 function updateList(person) {
+<<<<<<< Updated upstream
   setCharacters([...characters, person]);
+=======
+ postUser(person) 
+	.then(response => { 
+	  if (response.status == 201) {
+	    return response.json();
+	  } else {
+	    console.log("Failed to add user. Status:", response.status);
+   	  }
+        })
+        .then((newUser)  => {
+	  if (newUser) {
+	    setCharacters([...characters, newUser]);
+          }
+	})
+	.catch((error) => console.log(error));
+	
+}
+
+function fetchUsers() {
+    return fetch("http://localhost:8000/users"); 
+} 
+
+useEffect(() => { 
+    fetchUsers()
+	.then((res) => res.json())
+	.then((json) => setCharacters(json))
+	.catch((error) => { console.log(error));
+}, [] ); 
+
+function postUser(person) {
+    return fetch("http://localhost:8000/users", { 
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json", 
+      }, 
+      body: JSON.stringify(person), 
+   });  
+>>>>>>> Stashed changes
 }
 
 return (
